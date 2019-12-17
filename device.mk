87f0240ac3d5b5d588b6ff1b7894b1e77456ed67
TARKZiM <tom8476oo@gmail.com>
2019-12-18 01:06:12 +0800

    willow: Copy NFC Mifare permissions

--- device.mk
+++ device.mk
@@ -21,7 +21,8 @@ $(call inherit-product-if-exists, vendor/xiaomi/willow/willow-vendor.mk)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
 # NFC
 PRODUCT_PACKAGES += \
