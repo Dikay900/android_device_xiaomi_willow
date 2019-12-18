aac36d8b4dc2b9684b065df2ee6f266b9715aca7
TARKZiM <tom8476oo@gmail.com>
2019-12-18 09:30:47 +0800

    willow: Import Light HAL from sm6125-common
    * Build it as device-specific variant.

--- device.mk
+++ device.mk
@@ -24,6 +24,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.xiaomi_willow
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
